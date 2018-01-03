65d333c2caa267a1e44bc804c4c27ed2980eed59
Alessandro Canicatti <ale7canna@hotmail.it>
2018-01-03 20:11:10 +0100

    2 layers w/ wo/ F keys

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -5,14 +5,16 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BL 0
-#define _FL 1
+#define _FBL 1
+#define _IL 2
+#define _FIL 3
 
 #define _______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
-   * |Esc |F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|  -|  =|Backsp |~ ` |
+   * |Esc |  1|  2|  3|  4|  5|  6|  7|  8|  9| 0|  -|  =|Backsp |~ ` |
    * |----------------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
    * |----------------------------------------------------------------|
@@ -23,22 +25,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
-
-	//		| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|
-	//		KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   
-
-	//		| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10
-	//		KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, 
 [_BL] = KEYMAP_ANSI(
-  KC_ESC,  KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,  KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
+  KC_ESC,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
   KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,		KC_ENT	   ,KC_PGUP,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_PGDN, \
-  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        KC_RALT,MO(_FL),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        KC_RALT,MO(_FBL),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
+
+
+
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
-   * |   |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|F11|F12|Del    |Ins |
+   * |   |F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10 |F11|F12|Del    |Ins |
    * |----------------------------------------------------------------|
    * |     |VU-|MUT|VU+|   |   |   |   |   |   |   |   |   |     |Hme |
    * |----------------------------------------------------------------|
@@ -46,15 +45,63 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |        |   |   |	  |   |   |   |   |   |   |   |   McL|MsU|McR |
    * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |App |MsL|MsD|MsR |
+   * |_IL |    |    |                       |   |   |App |MsL|MsD|MsR |
    * `----------------------------------------------------------------'
    *
    * App stands for windows context menu
    */
-[_FL] = KEYMAP_ANSI(
-  _______, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  , KC_6  , KC_7  , KC_8  , KC_9  , KC_0  , KC_F11, KC_F12, KC_DEL, KC_INS ,  \
+[_FBL] = KEYMAP_ANSI(
+  _______, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 , KC_F7 , KC_F8 , KC_F9 , KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
   _______,KC_VOLD,KC_MUTE,KC_VOLU,_______,_______,_______,_______,_______,_______,_______,_______,_______, _______,KC_HOME, \
   _______, BL_DEC,BL_TOGG, BL_INC,_______,_______,_______,_______,_______,_______,_______,_______,        _______,KC_END, \
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
-  _______,_______,_______,                 _______,               _______,_______,KC_APP ,KC_MS_L,KC_MS_D, KC_MS_R),
+  TO(_IL),_______,_______,                 _______,               _______,_______,KC_APP ,KC_MS_L,KC_MS_D, KC_MS_R),
+
+
+
+/* Keymap _IL: Inverted Layer - Number keys are Function keys by default
+   * ,----------------------------------------------------------------.
+   * |Esc |F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Backsp |~ ` |
+   * |----------------------------------------------------------------|
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
+   * |----------------------------------------------------------------|
+   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |----------------------------------------------------------------|
+   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
+   * |----------------------------------------------------------------|
+   * |Ctrl|Win |Alt |        Space          |Alt|FIL|Ctrl|Lef|Dow|Rig |
+   * `----------------------------------------------------------------'
+   */
+[_IL] = KEYMAP_ANSI(
+  KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,    KC_F11,  KC_F12, KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
+  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
+  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,		KC_ENT	   ,KC_PGUP,  \
+  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_PGDN, \
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        KC_RALT,MO(_FIL),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
+
+
+
+  /* Keymap _FIL: Function Inverted Layer
+   * ,----------------------------------------------------------------.
+   * |   |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Del    |Ins |
+   * |----------------------------------------------------------------|
+   * |     |VU-|MUT|VU+|   |   |   |   |   |   |   |   |   |     |Hme |
+   * |----------------------------------------------------------------|
+   * |      |Bl-|BL |BL+|   |   |   |   |   |   |   |   |        |End |
+   * |----------------------------------------------------------------|
+   * |        |   |   |	  |   |   |   |   |   |   |   |   McL|MsU|McR |
+   * |----------------------------------------------------------------|
+   * |_BL |    |    |                       |   |   |App |MsL|MsD|MsR |
+   * `----------------------------------------------------------------'
+   *
+   * App stands for windows context menu
+   */
+[_FIL] = KEYMAP_ANSI(
+  _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS, KC_EQL, KC_DEL, KC_INS ,  \
+  _______,KC_VOLD,KC_MUTE,KC_VOLU,_______,_______,_______,_______,_______,_______,_______,_______,_______, _______,KC_HOME, \
+  _______, BL_DEC,BL_TOGG, BL_INC,_______,_______,_______,_______,_______,_______,_______,_______,        _______,KC_END, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
+  TO(_BL),_______,_______,                 _______,               _______,_______,KC_APP ,KC_MS_L,KC_MS_D, KC_MS_R),
+
+
 };
