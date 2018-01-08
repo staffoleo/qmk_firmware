2b3d2cfa81f676eba02af56c150e93647e5ba876
Alessandro Canicatti <ale7canna@hotmail.it>
2018-01-08 18:30:45 +0100

    CAPS Lock and TAB support modded taps
    # Conflicts:
    #	keyboards/tada68/keymaps/default/keymap.c

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ enum emoticons {
 };
 
 
-#define trigger_time 400
+#define trigger_time 200
 
 #define T1 M(1)
 #define T2 M(2)
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * |Esc |F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|  -|  =|Backsp |~ ` |
    * |----------------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
+   * |C+S,Ta| Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
    * |----------------------------------------------------------------|
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |CT,CAPS|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
    * |----------------------------------------------------------------|
@@ -59,8 +59,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//		KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
 [_BL] = KEYMAP_ANSI(
   KC_ESC,  T1, 		T2, 		T3, 		T4, 		T5, 		T6, 		T7, 		T8, 		T9, 		T10, 		T11, 		T12, KC_BSPC,KC_GRV, \
-  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
-  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,		KC_ENT	   ,KC_PGUP,  \
+  MT(MOD_LCTL | MOD_LSFT, KC_TAB),  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
+  MT(MOD_LCTL, KC_CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,		KC_ENT	   ,KC_PGUP,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_PGDN, \
   KC_LCTL, FN_WIN,KC_LALT,                KC_SPC,                        KC_RALT,FN_APP,KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
 
@@ -86,7 +86,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LENNY  ,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
   LENNY  ,_______,_______,                 _______,               _______,_______,KC_APP ,KC_MS_L,KC_MS_D, KC_MS_R),
 };
-
 
 
 
@@ -135,9 +134,6 @@ void tap(uint16_t keycode){
     register_code(keycode);
     unregister_code(keycode);
 };
-
-
-
 
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
