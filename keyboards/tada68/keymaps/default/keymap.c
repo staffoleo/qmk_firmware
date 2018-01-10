208452d189c08c4512a261f43f823869ca9fb258
Canicatti Alessandro <alessandro.canicatti@tcpos.com>
2018-01-10 14:09:43 +0100

    Good status

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -12,9 +12,8 @@
 #define _FL 1
 
 enum emoticons {
-	LENNY = SAFE_RANGE,
-	DWNHRT,
-	SHRUG
+	LED_UP = SAFE_RANGE,
+	LED_DOWN
 };
 
 
@@ -47,7 +46,7 @@ static uint16_t key_timer;
 #define MY_CAP MT(MOD_LCTL, KC_CAPS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: (Base Layer) Default Layer
+    /* Keymap _BL: (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
    * |Esc |F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|  -|  =|Backsp |~ ` |
    * |----------------------------------------------------------------|
@@ -61,19 +60,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 
-	//		| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|
-	//		KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,
+    //		| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|
+    //		KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,
 
-	//		| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10
-	//		KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-[_BL] = KEYMAP_ANSI(
-  KC_ESC,    T1, 		T2, 		T3, 		T4, 		T5, 		T6, 		T7, 		T8, 		T9, 		T10, 		T11, 		T12, KC_BSPC,KC_GRV, \
-  MY_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
-  MY_CAP,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,		KC_ENT	   ,KC_PGUP,  \
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_PGDN, \
-  KC_LCTL, MY_WIN,KC_LALT,                KC_SPC,                        KC_RALT,FN_APP,KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
+    //		| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10
+    //		KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+    [_BL] = KEYMAP_ANSI(
+        KC_ESC, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, KC_BSPC, KC_GRV,
+        MY_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+        MY_CAP, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGUP,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN,
+        KC_LCTL, MY_WIN, KC_LALT, KC_SPC, KC_RALT, FN_APP, KC_RCTRL, KC_LEFT, KC_DOWN, KC_RGHT),
 
-  /* Keymap _FL: Function Layer
+    /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
    * |   |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|F11|F12|Del    |Ins |
    * |----------------------------------------------------------------|
@@ -81,22 +80,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |      |Bl-|BL |BL+|   |   |   |MsL|MsD|MsR|   |   |        |End |
    * |----------------------------------------------------------------|
-   * |        |   |   |	  |   |   |   |   |   |   |   |   |  |PGUP|   |
+   * |        |   |   |	  |   |   |   |   |   |   |   |   |  |PGUP|HOM|
    * |----------------------------------------------------------------|
    * |    |    |    |                       |   |   |App|HOME|PGDN|END|
    * `----------------------------------------------------------------'
    *
    * App stands for windows context menu
    */
-[_FL] = KEYMAP_ANSI(
-  _______, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  , KC_6  , KC_7  , KC_8  , KC_9  , KC_0  , KC_F11, KC_F12, KC_DEL, KC_INS ,  \
-  _______,KC_VOLD,KC_MUTE,KC_VOLU,_______,_______,_______,_______,KC_BTN1,KC_MS_U,KC_BTN2,_______,_______, _______,KC_HOME, \
-  _______, BL_DEC,BL_TOGG, BL_INC,_______,_______,_______,_______,KC_MS_L,KC_MS_D,KC_MS_R,_______,        _______,KC_END, \
-  LENNY  ,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_PGUP,_______, \
-  LENNY  ,_______,_______,                 _______,               _______,_______,KC_APP ,KC_HOME,KC_PGDN,KC_END),
+    [_FL] = KEYMAP_ANSI(
+        _______, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_F11, KC_F12, KC_DEL, KC_INS,
+        _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, KC_HOME,
+        _______, BL_DEC, BL_TOGG, BL_INC, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_END,
+        LED_UP , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME,
+       LED_DOWN, _______, _______, _______, _______, _______, KC_APP, KC_HOME, KC_PGDN, KC_END),
 };
-
-
 
 void led_set_user(uint8_t usb_led) {
 	if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
@@ -108,6 +105,8 @@ void led_set_user(uint8_t usb_led) {
     }
 }
 
+int led_status = 0;
+
 void tap(uint16_t keycode)
 {
   register_code(keycode);
@@ -116,28 +115,20 @@ void tap(uint16_t keycode)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
-		case LENNY:			// ( ͡° ͜ʖ ͡°)
+		case LED_UP:
 			if(record->event.pressed){
-				set_unicode_input_mode(UC_WIN);
-				register_code(KC_LSFT);
-				tap(KC_9);										// Head
-				unregister_code(KC_LSFT);
-				tap(KC_SPC);									// Space
-				process_unicode((0x0361|QK_UNICODE), record);	// Eyebrow
-				process_unicode((0x00B0|QK_UNICODE), record);	// Eye
-				tap(KC_SPC);
-				process_unicode((0x035C|QK_UNICODE), record);	// Mouth
-				process_unicode((0x0296|QK_UNICODE), record);	// Nose
-				tap(KC_SPC);
-				process_unicode((0x0361|QK_UNICODE), record);	// Eyebrow
-				process_unicode((0x00B0|QK_UNICODE), record);	// Eye
-				register_code(KC_LSFT);
-				tap(KC_0);										// Head
-				unregister_code(KC_LSFT);
-			}
-			return false;
+        PORTB |= (1<<6);
+      }
+      return false;
 			break;
-	}
+    case LED_DOWN:
+      if (record->event.pressed)
+      {
+        PORTB &= ~(1 << 6);
+      }
+      return false;
+      break;
+  }
 	return true;
 };
 
@@ -321,7 +312,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
      else
      {
        layer_off(_FL);
-       if (timer_elapsed(key_timer) > trigger_time)
+       if (timer_elapsed(key_timer) < trigger_time)
        {
          register_code(KC_LGUI);
          unregister_code(KC_LGUI);
