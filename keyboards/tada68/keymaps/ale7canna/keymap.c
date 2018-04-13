1c39851d280d9da408ac2cbcfbf4879fa50784c7
Canicatti Alessandro <alessandro.canicatti@tcpos.com>
2018-04-13 18:16:32 +0200

    E/Gui, I/Gui Gui keys are under middle finger on upper row

--- keyboards/tada68/keymaps/ale7canna/keymap.c
+++ keyboards/tada68/keymaps/ale7canna/keymap.c
@@ -11,25 +11,27 @@
 #define MY_K MT(MOD_RCTL, KC_K)
 #define MY_S MT(MOD_LALT, KC_S)
 #define MY_L MT(MOD_RALT, KC_L)
+#define MY_E MT(MOD_LGUI, KC_E)
+#define MY_I MT(MOD_RGUI, KC_I)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap BASE: (Base Layer) Default Layer
    * ,----------------------------------------------------------------------------------------------.
    * |Esc   |   1|    2|    3|    4|    5|    6|    7|    8|    9|    0|    -|    =|  Backsp |  ~ ` |
    * |----------------------------------------------------------------------------------------------|
-   * |Tab    |    Q|    W|    E|    R|    T|    Y|    U|    I|    O|    P|    [|    ]|    \  |  Del |
+   * |Tab    |    Q|    W|E/GUI|    R|    T|    Y|    U|I/GUI|    O|    P|    [|    ]|    \  |  Del |
    * |----------------------------------------------------------------------------------------------|
    * |CAPS     |    A|S/ALT|D/CTL|F/SFT|    G|    H|J/SFT|K/CTL|L/ALT|    ;|    '|  Return   |  PgUp|
    * |----------------------------------------------------------------------------------------------|
    * |          |    Z|    X|    C|    V|    B|    N|    M|    ,|    .|    /|          |   Up|  PgDn|
    * |----------------------------------------------------------------------------------------------|
-   * |Ctrl  |  Win |  Alt |          Space                    |  Alt|   FN|  Ctrl|  Lef|  Dow|  Rig |
+   * |      |      |      |          Space                    |     |   FN|     |  Lef|  Dow|  Rig |
    * `----------------------------------------------------------------------------------------------'
    */
 [BASE] = KEYMAP_ANSI(
   KC_ESC  ,  KC_1  ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,   KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , KC_MINS, KC_EQL , KC_BSPC,   KC_GRV, \
-  KC_TAB  ,  KC_Q  ,   KC_W ,   KC_E ,   KC_R ,   KC_T ,   KC_Y ,   KC_U ,   KC_I ,   KC_O ,   KC_P , KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL, \
+  KC_TAB  ,  KC_Q  ,   KC_W ,   MY_E ,   KC_R ,   KC_T ,   KC_Y ,   KC_U ,   MY_I ,   KC_O ,   KC_P , KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL, \
   KC_CAPS ,  KC_A  ,   MY_S ,   MY_D ,   MY_F ,   KC_G ,   KC_H ,   MY_J ,   MY_K ,   MY_L , KC_SCLN, KC_QUOT,           KC_ENT,  KC_PGUP, \
   _______ ,            KC_Z ,   KC_X ,   KC_C ,   KC_V ,   KC_B ,   KC_N ,   KC_M , KC_COMM, KC_DOT , KC_SLSH, _______,   KC_UP,  KC_PGDN, \
-  _______ , KC_LGUI, _______,                KC_SPC                               , _______, MO(_FL), _______, KC_LEFT, KC_DOWN,  KC_RGHT),
+  _______ , _______, _______,                KC_SPC                               , _______, MO(_FL), _______, KC_LEFT, KC_DOWN,  KC_RGHT),
 };
